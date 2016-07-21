2581442dc153f92e3b3cbc53d132c27361fe8895
lzyang1995 <lzyang1995@outlook.com>
2016-07-21 15:39:12 +0800

    test the performance of store_record
    test the performance of store_record

---
+++ time_test.c
@@ -0,0 +1,33 @@
+//test the performance of store_record
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <db.h>
+#include <time.h>
+#include <inttypes.h>
+
+#define BILLION 1000000000UL
+
+int main(int argc, char **argv)
+{
+	uint64_t key;
+	struct timespec start_time, end_time;
+	uint64_t diff;
+	FILE *fp = fopen(argv[1], "w");
+	
+	initialize_db(0);
+	
+	while(1)
+	{
+		clock_gettime(CLOCK_MONOTONIC, &start_time);
+		store_record(sizeof(uint64_t), &key, sizeof(uint64_t), &key);
+		clock_gettime(CLOCK_MONOTONIC, &end_time);
+		diff = BILLION * (end_time.tv_sec - start_time.tv_sec) + end_time.tv_nsec - start_time.tv_nsec;
+		fprintf(fp, "%"PRIu64"\n", diff);
+		
+		key++;
+	}
+	
+	return 0;
+}
