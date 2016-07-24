e9b5066534dd48a8ca8a363be693ed9f2cb74ece
lzyang1995 <lzyang1995@outlook.com>
2016-07-24 20:31:50 +0800

    test performance

--- main.c
+++ main.c
@@ -4,40 +4,40 @@
 #include <db.h>
 #include <unistd.h>
 #include <inttypes.h>
+#include <time.h>
+
+#define THREAD_NUM 20
+#define COUNT 50000000UL
+#define BILLION 1000000000UL
+#define FILENAME "time.txt"
 
 void * func(void *arg);
 void * func2(void *arg);
 
-uint64_t key;
-pthread_spinlock_t spinlock;
+uint64_t key = 0;
+pthread_spinlock_t spinlock, filelock;
 //const char *data = "become legendary";
 
 int main()
 {
 	int i, ret;
-	pthread_t pid[5];
-	pthread_t retrieve_id;
+	pthread_t pid[THREAD_NUM];
+	//pthread_t retrieve_id;
+	FILE *fp = fopen(FILENAME, "w");
 
 	initialize_db(0);
-	key = 0;
 	pthread_spin_init(&spinlock, PTHREAD_PROCESS_PRIVATE);
+	pthread_spin_init(&filelock, PTHREAD_PROCESS_PRIVATE);
 
-	for(i = 0;i < 5;i++)
-		if(ret = pthread_create(&pid[i], NULL, func, NULL))
+	for(i = 0;i < THREAD_NUM;i++)
+		if(ret = pthread_create(&pid[i], NULL, func, (void *)fp)
 		{
 			fprintf(stderr, "Thread creation failed: %d\n", i);
 			exit(1);
 		}
-
-	usleep(3000000L);
-
-	if(ret = pthread_create(&retrieve_id, NULL, func2, NULL))
-	{
-		fprintf(stderr, "Thread func2 creation failed");
-		exit(1);
-	}
-
-	while(1);
+		
+	for(i = 0;i < THREAD_NUM;i++)
+		pthread_join(pid[i], NULL);
 
 	return 0;
 }
@@ -45,14 +45,30 @@ int main()
 void * func(void *arg)
 {
 	uint64_t thekey;
+	FILE *fp = (FILE *)arg;
+	struct timespec start_time, end_time;
+	uint64_t diff;
 
 	while(1)
 	{
 		pthread_spin_lock(&spinlock);
+		if(key = COUNT)
+		{
+			pthread_spin_unlock(&spinlock);
+			break;
+		}
+		
 		thekey = key++;
 		pthread_spin_unlock(&spinlock);
 
+		clock_gettime(CLOCK_MONOTONIC, &start_time);
 		store_record(sizeof(uint64_t), &thekey, sizeof(uint64_t), &thekey);
+		clock_gettime(CLOCK_MONOTONIC, &end_time);
+		diff = BILLION * (end_time.tv_sec - start_time.tv_sec) + end_time.tv_nsec - start_time.tv_nsec;
+		
+		pthread_spin_lock(&filelock);
+		fprintf(fp, "%"PRIu64"\n", diff);
+		pthread_spin_unlock(&filelock);
 	}
 }
 
